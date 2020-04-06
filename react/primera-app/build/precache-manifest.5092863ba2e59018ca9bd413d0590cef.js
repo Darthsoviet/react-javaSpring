2441d439726044371b0fe540e20c60c4c9db96fc
Darthsoviet <31465914+Darthsoviet@users.noreply.github.com>
2020-04-05 19:58:55 -0500

    build react

---
+++ react/primera-app/build/precache-manifest.5092863ba2e59018ca9bd413d0590cef.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08985e1d0586210ea5302f4171574e53",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fe3be49cf1530e5b3421",
+    "url": "./static/css/main.f42e05a3.chunk.css"
+  },
+  {
+    "revision": "9c7349d127cb2c490b63",
+    "url": "./static/js/2.2a5cbf9d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.2a5cbf9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe3be49cf1530e5b3421",
+    "url": "./static/js/main.f83c04d8.chunk.js"
+  },
+  {
+    "revision": "4a4a8e6aae026e550698",
+    "url": "./static/js/runtime-main.3fc91fa2.js"
+  },
+  {
+    "revision": "619a9c54cb370c1031e3365ebb4e376b",
+    "url": "./static/media/boat-logistic-transportation-deleivery-vehicle-ship-container-parcel-10-20541.619a9c54.png"
+  },
+  {
+    "revision": "4cc9ac5fbfa1cba5109b3ef21e60aa7f",
+    "url": "./static/media/box-package-parcel-logistic-delivery-unpack-open-shipping-5-20583.4cc9ac5f.png"
+  },
+  {
+    "revision": "2ca884530d6e08f8540700e76c3a63cb",
+    "url": "./static/media/css3-8-1175200.2ca88453.svg"
+  },
+  {
+    "revision": "46d7a1547e9f13fb51d6a2c842838d36",
+    "url": "./static/media/git-16-1175195.46d7a154.svg"
+  },
+  {
+    "revision": "eb0bfe64ddcaa0b4c3be19b19541b584",
+    "url": "./static/media/html5-42-1175210.eb0bfe64.svg"
+  },
+  {
+    "revision": "27c6b445b630ba466207d9ec36b5e536",
+    "url": "./static/media/java-58-1174951.27c6b445.svg"
+  },
+  {
+    "revision": "de79c8f14c2b0d692ae215d7e6eccd54",
+    "url": "./static/media/javascript-24-1174950.de79c8f1.svg"
+  },
+  {
+    "revision": "fede0b573824dce14b358ade72e00e50",
+    "url": "./static/media/minimi.fede0b57.png"
+  },
+  {
+    "revision": "c02ade79e7034732a856b802471a5f31",
+    "url": "./static/media/mysql-21-1174941.c02ade79.svg"
+  },
+  {
+    "revision": "81303c6e10851351bec2007e2a67d80e",
+    "url": "./static/media/server.81303c6e.svg"
+  }
+]);
