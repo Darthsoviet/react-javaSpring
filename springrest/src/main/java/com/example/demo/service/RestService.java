0447b0043d4cdafdd65ef94186ffecdf6a944cd0
Darthsoviet <31465914+Darthsoviet@users.noreply.github.com>
2020-04-21 20:40:15 -0500

    v1.0.1

--- springrest/src/main/java/com/example/demo/service/RestService.java
+++ springrest/src/main/java/com/example/demo/service/RestService.java
@@ -10,14 +10,13 @@ import com.example.demo.dominio.Item;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,43 +31,71 @@ public class RestService {
     @Autowired
     private ItemDao item;
 
-    // ______________________________________________________________________________________________________________________
+    // ___________________________________________________________GET__/________________________________________________________
 
-    @RequestMapping(produces = "application/json", method = RequestMethod.GET, path = "/")
+    @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET, path = "/")
+    @CrossOrigin(allowCredentials = "true", allowedHeaders = "*", methods = { RequestMethod.DELETE, RequestMethod.POST,
+            RequestMethod.GET, RequestMethod.OPTIONS, RequestMethod.PUT }, origins = "*")
     public ResponseEntity<String> bienvenida() {
 
         return ResponseEntity.ok().body("Hola mundo");
 
     }
 
+   
+    // ___________________________________________________________GET__/api/item________________________________________________________
+
+    @RequestMapping(value = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @CrossOrigin(allowCredentials = "true", allowedHeaders = "*", methods = { RequestMethod.DELETE, RequestMethod.POST,
-            RequestMethod.GET, RequestMethod.OPTIONS, RequestMethod.PUT }, origins = "*")
-
-    @RequestMapping(value = "/api/item", method = RequestMethod.GET, produces = "application/json")
-
+        RequestMethod.GET, RequestMethod.OPTIONS, RequestMethod.PUT }, origins = "*")
     public ResponseEntity<List<Item>> getData() {
 
         List<Item> items = (List<Item>) item.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(items);
     }
 
+
+    // ___________________________________________________________PUT__/api/item________________________________________________________
+
+    @RequestMapping(value = "/api/item", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
     @CrossOrigin(allowCredentials = "true", allowedHeaders = "*", methods = { RequestMethod.DELETE, RequestMethod.POST,
             RequestMethod.GET, RequestMethod.OPTIONS, RequestMethod.PUT }, origins = "*")
-
-    @RequestMapping(value = "/api/item", method = RequestMethod.PUT, produces = "application/json")
-
     public ResponseEntity<Item> putData(@RequestBody Item item) {
-        this.item.save(item);
 
+        this.item.save(item);
         return ResponseEntity.status(HttpStatus.OK).body(item);
     }
 
+
+    // ___________________________________________________________POST__/api/item________________________________________________________
+
+    @RequestMapping(value = "/api/item", method = RequestMethod.POST, produces = "application/json", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(allowCredentials = "true", allowedHeaders = "*", methods = { RequestMethod.DELETE, RequestMethod.POST,
+        RequestMethod.GET, RequestMethod.OPTIONS, RequestMethod.PUT }, origins = "*")
+    public ResponseEntity<Item> postData(@RequestBody Item item) {
+
+        item.setIdItem(item.getNombre());
+        item = this.item.save(item);
+        return new ResponseEntity<Item>(item, HttpStatus.CREATED);
+
+}
+    // ___________________________________________________________OPTIONS__/api/item________________________________________________________
+
+    @RequestMapping(value = "/api/item", method = RequestMethod.OPTIONS)
+    @CrossOrigin(allowCredentials = "true", allowedHeaders = "*", methods = { RequestMethod.DELETE, RequestMethod.POST,
+        RequestMethod.GET, RequestMethod.OPTIONS, RequestMethod.PUT }, origins = "*")
+    public ResponseEntity<Object> options() {
+    return ResponseEntity.ok().build();
+}
+
+
+    // ___________________________________________________________DELETE__/api/item/id________________________________________________________
+
+    @RequestMapping(value = "/api/item/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @CrossOrigin(allowCredentials = "true", allowedHeaders = "*", methods = { RequestMethod.DELETE, RequestMethod.POST,
             RequestMethod.GET, RequestMethod.OPTIONS, RequestMethod.PUT }, origins = "*")
-
-    @RequestMapping(value = "/api/item/{id}", method = RequestMethod.GET, produces = "application/json")
-
     public Item getData(@PathVariable String id) {
+
         Item item = new Item();
         item.setIdItem(id);
 
@@ -77,42 +104,22 @@ public class RestService {
     }
 
 
-
+    // ___________________________________________________________GET__/api/item/id________________________________________________________
     
+    @RequestMapping(value = "/api/item/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
     @CrossOrigin(allowCredentials = "true", allowedHeaders = "*", methods = { RequestMethod.DELETE, RequestMethod.POST,
             RequestMethod.GET, RequestMethod.OPTIONS, RequestMethod.PUT }, origins = "*")
-
-
-    @RequestMapping(value = "/api/item/{id}", method = RequestMethod.DELETE, produces = "application/json")
-
     public ResponseEntity<String> deleteData(@PathVariable String id) {
 
-        Item itemTemp=new Item();
+        Item itemTemp = new Item();
         itemTemp.setIdItem(id);
-        Optional<Item> retorno=this.item.findById(itemTemp.getIdItem());
+        Optional<Item> retorno = this.item.findById(itemTemp.getIdItem());
         this.item.delete(retorno.get());
-      
-        
+
         return ResponseEntity.status(HttpStatus.OK).body(id);
     }
 
-    @CrossOrigin(allowCredentials = "true", allowedHeaders = "*", methods = { RequestMethod.DELETE, RequestMethod.POST,
-            RequestMethod.GET, RequestMethod.OPTIONS, RequestMethod.PUT }, origins = "*")
-    @RequestMapping(value = "/api/item", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
 
-    public ResponseEntity<Item> postData(@RequestBody Item item) {
-        item.setIdItem(item.getNombre());
-        item = this.item.save(item);
-        return new ResponseEntity<Item>(item, HttpStatus.CREATED);
-
-    }
-
-    @CrossOrigin(allowCredentials = "true", allowedHeaders = "*", methods = { RequestMethod.DELETE, RequestMethod.POST,
-            RequestMethod.GET, RequestMethod.OPTIONS, RequestMethod.PUT }, origins = "*")
-    @RequestMapping(value = "/api/item", method = RequestMethod.OPTIONS)
-
-    public ResponseEntity<Object> options() {
-        return ResponseEntity.ok().build();
-    }
+  
 
 }
